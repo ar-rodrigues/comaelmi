0bdd8c622dcd8231250928eccef2435b18c3c793
Alisson Rodrigues <68715629+ar-rodrigues@users.noreply.github.com>
2024-03-06 10:04:30 +0000

    listProduct fixed

--- components/listOfProducts.js
+++ components/listOfProducts.js
@@ -7,17 +7,23 @@ import ProductItem from './ui-user/productItem';
 export default function ListOfProducts({ products, deleteProduct, error, setError, cartList, setCartList }) {
   const { pathname: admin } = useRouter();
 
+  // Check if products is an array
+  if (!Array.isArray(products)) {
+    // Return null or an appropriate fallback if products is not an array
+    return null;
+  }
+
   return (
     <div>
       {products.map((product) => (
-        <div key={product._id} >
+        <div key={product._id}>
           {admin === "/admin" ? (
-          <div >
-            <Product productData={product} error={error} setError={setError} deleteProduct={deleteProduct} />
+            <div>
+              <Product productData={product} error={error} setError={setError} deleteProduct={deleteProduct} />
             </div>
           ) : (
-          <div>
-            <ProductItem product={product} cartList={cartList} setCartList={setCartList} />
+            <div>
+              <ProductItem product={product} cartList={cartList} setCartList={setCartList} />
             </div>
           )}
         </div>
